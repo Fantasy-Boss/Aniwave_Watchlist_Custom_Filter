dc6870ea1715118119971cf136124428f09da5bd
Fantasy Boss <apurbo.kumar.official@gmail.com>
2022-10-08 20:44:34 +0600

    1st

---
+++ 9anime Watch-List Custom Filter.js
@@ -0,0 +1,225 @@
+// ==UserScript==
+// @name           9anime Watch-List Custom Filter
+// @namespace      https://greasyfork.org/en/users/957626-fantasy-boss
+// @homepageURL    https://github.com/Fantasy-Boss/9anime-Watch-List-Custom-Filter
+// @supportURL     https://github.com/Fantasy-Boss/9anime-Watch-List-Custom-Filter/issues/new
+// @version        1.0.0
+// @description    9anime Watch-List Custom Filter
+// @author         Fantasy Boss
+// @icon           https://www.google.com/s2/favicons?domain=9anime.id
+// @match          *://9anime.to/user/watch-list*
+// @match          *://9anime.id/user/watch-list*
+// @match          *://9anime.gs/user/watch-list*
+// @match          *://9anime.pl/user/watch-list*
+// @grant          GM_addStyle
+// @require        https://code.jquery.com/jquery-3.6.1.min.js
+// @compatible     chrome
+// @compatible     firefox
+// @run-at         document-end
+// @license        MIT License
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+
+    window.addEventListener('load', async() => {
+        // Add The Custom HTML and Style
+        GM_addStyle(`#custom-filter {
+            background: #212121;
+            margin-bottom: 20px;
+            margin-top: 10px;
+            padding: 10px;
+            border-radius: 5px;
+            flex-wrap: wrap;
+        } #custom-filter button > span {
+            pointer-events:none
+        } .cf-active, .cf-active:hover, .cf-active:focus, .cf-active:active {
+            background-color: #3d1576;
+            color: #fff;
+        }
+        `)
+        let html = `
+        <div id="custom-filter" class="d-flex align-items-center">
+          <span class="h5 ml-1 mr-4 mb-0">Custom Filter:</span>
+          <button class="btn btn-bg btn-primary d-flex align-items-center mr-3" id="com-sub">
+            <span>Completed (Sub)</span>
+          </button>
+          <button class="btn btn-bg btn-primary d-flex align-items-center mr-3" id="com-dub">
+            <span>Completed (Dub)</span>
+          </button>
+          <button class="btn btn-bg btn-primary d-flex align-items-center mr-3" id="air-sub">
+            <span>Releasing (Sub)</span>
+          </button>
+          <button class="btn btn-bg btn-primary d-flex align-items-center mr-3" id="air-dub">
+            <span>Releasing (Dub)</span>
+          </button>
+          <button class="btn btn-bg btn-primary d-flex align-items-center" id="reset">
+            <span>Reset Filter</span>
+          </button>
+        </div>
+        `
+        $('#body > div.profile-container > div > aside.main > div.watchlist.scaff.items').before(html)
+
+        // Clicked Functions
+        const reset = async()=> {
+            $('#custom-filter button').removeClass('cf-active')
+            $('#body > div.profile-container > div > aside.main > div.watchlist.scaff.items .item').removeClass('d-none')
+        }
+        const comSub = async()=> {
+            let arr = $('#body > div.profile-container > div > aside.main > div.watchlist.scaff.items .item')
+            arr.map((i)=> {
+                let ep_count = 0
+                let com_count = 0
+                let span_arr = $(arr[i]).find(`.info .detail .ep span`)
+                if ($(span_arr).length > 1) {
+                    span_arr.map((o)=> {
+                        if (($(span_arr[o]).text()).includes('EPS')) {
+                            ep_count = parseInt($(span_arr[o]).text())
+                        }
+                        if ($(span_arr[o]).find(`i`).attr('class') === 'fa-solid fa-closed-captioning') {
+                            com_count = parseInt($(span_arr[o]).text())
+                        }
+                    })
+                }
+                else {
+                    if (($(span_arr).text()).includes('EPS')) {
+                        ep_count = parseInt($(span_arr).text())
+                    } else {
+                        ep_count = 9999999999999999999999
+                        if ($(span_arr).find(`i`).attr('class') === 'fa-solid fa-closed-captioning') {
+                            com_count = parseInt($(span_arr).text())
+                        }
+                    }
+                }
+                if (ep_count !== com_count) $(arr[i]).addClass('d-none')
+            })
+        }
+        const comDub = async()=> {
+            let arr = $('#body > div.profile-container > div > aside.main > div.watchlist.scaff.items .item')
+            arr.map((i)=> {
+                let ep_count = 0
+                let com_count = 0
+                let span_arr = $(arr[i]).find(`.info .detail .ep span`)
+                if ($(span_arr).length > 1) {
+                    span_arr.map((o)=> {
+                        if (($(span_arr[o]).text()).includes('EPS')) {
+                            ep_count = parseInt($(span_arr[o]).text())
+                        }
+                        if ($(span_arr[o]).find(`i`).attr('class') === 'fa-solid fa-microphone') {
+                            com_count = parseInt($(span_arr[o]).text())
+                        }
+                    })
+                }
+                else {
+                    if (($(span_arr).text()).includes('EPS')) {
+                        ep_count = parseInt($(span_arr).text())
+                    } else {
+                        ep_count = 9999999999999999999999
+                        if ($(span_arr).find(`i`).attr('class') === 'fa-solid fa-microphone') {
+                            com_count = parseInt($(span_arr).text())
+                        }
+                    }
+                }
+                if (ep_count !== com_count) $(arr[i]).addClass('d-none')
+            })
+        }
+
+        const airSub = async()=> {
+            let arr = $('#body > div.profile-container > div > aside.main > div.watchlist.scaff.items .item')
+            arr.map((i)=> {
+                let ep_count = 0
+                let com_count = 0
+                let span_arr = $(arr[i]).find(`.info .detail .ep span`)
+                if ($(span_arr).length > 1) {
+                    span_arr.map((o)=> {
+                        if (($(span_arr[o]).text()).includes('EPS')) {
+                            ep_count = parseInt($(span_arr[o]).text())
+                        }
+                        if ($(span_arr[o]).find(`i`).attr('class') === 'fa-solid fa-closed-captioning') {
+                            com_count = parseInt($(span_arr[o]).text())
+                        }
+                    })
+                }
+                else {
+                    if (($(span_arr).text()).includes('EPS')) {
+                        ep_count = parseInt($(span_arr).text())
+                    } else {
+                        ep_count = 9999999999999999999999
+                        if ($(span_arr).find(`i`).attr('class') === 'fa-solid fa-closed-captioning') {
+                            com_count = parseInt($(span_arr).text())
+                        }
+                    }
+                }
+                if (ep_count === com_count || com_count === 0) $(arr[i]).addClass('d-none')
+            })
+        }
+        const airDub = async()=> {
+            let arr = $('#body > div.profile-container > div > aside.main > div.watchlist.scaff.items .item')
+            arr.map((i)=> {
+                let ep_count = 0
+                let com_count = 0
+                let span_arr = $(arr[i]).find(`.info .detail .ep span`)
+                if ($(span_arr).length > 1) {
+                    span_arr.map((o)=> {
+                        if (($(span_arr[o]).text()).includes('EPS')) {
+                            ep_count = parseInt($(span_arr[o]).text())
+                        }
+                        if ($(span_arr[o]).find(`i`).attr('class') === 'fa-solid fa-microphone') {
+                            com_count = parseInt($(span_arr[o]).text())
+                        }
+                    })
+                }
+                else {
+                    if (($(span_arr).text()).includes('EPS')) {
+                        ep_count = parseInt($(span_arr).text())
+                    } else {
+                        ep_count = 9999999999999999999999
+                        if ($(span_arr).find(`i`).attr('class') === 'fa-solid fa-microphone') {
+                            com_count = parseInt($(span_arr).text())
+                        }
+                    }
+                }
+                if (ep_count === com_count || com_count === 0) $(arr[i]).addClass('d-none')
+            })
+        }
+
+        // Clicked Events
+        $('#com-sub').click((e)=> {
+            reset()
+            $(e.target).addClass('cf-active')
+            comSub()
+        })
+        $('#com-dub').click((e)=> {
+            reset()
+            $(e.target).addClass('cf-active')
+            comDub()
+        })
+        $('#air-sub').click((e)=> {
+            reset()
+            $(e.target).addClass('cf-active')
+            airSub()
+        })
+        $('#air-dub').click((e)=> {
+            reset()
+            $(e.target).addClass('cf-active')
+            airDub()
+        })
+        $('#reset').click(reset)
+
+    }, false);
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,93 @@
+&nbsp;  
+
+<img src="https://raw.githubusercontent.com/Fantasy-Boss/9anime-Downloader/main/images/icon_x100.png">  
+
+9anime Watch-List Custom Filter
+=================
+
+#### Watch-List Custom Filter for 9anime. \<BETA\>
+
+* * *
+
+### Features:
+
+1\. Download Whole Season with One click.  
+2\. Download options: Sub - Dub - Video Quality.  
+3\. Fully Free, no Ads, no Virus, 100% Safe.  
+4\. Scrapping with progress bar.  
+5\. One click IDM Queue.  
+
+* * *
+
+### Supported sites list:
+
+9anime.to - <a href="https://9anime.to" target="_blank">9anime.to</a>  
+9anime.id - <a href="https://9anime.id" target="_blank">9anime.id</a>  
+9anime.gs - <a href="https://9anime.gs" target="_blank">9anime.gs</a>  
+9anime.pl - <a href="https://9anime.pl" target="_blank">9anime.pl</a>  
+
+* * *
+
+### Setup:
+
+1\. First you need a Browser Extension for managing [UserScripts](https://en.wikipedia.org/wiki/Userscript) (Skip this step if you already have one):   
+\~\~\~ Chrome: [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) or [Violentmonkey](https://chrome.google.com/webstore/detail/violent-monkey/jinjaccalgkegednnccohejagnlnfdag)  
+\~\~\~ Firefox: [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/), [Tampermonkey](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/), or [Violentmonkey](https://addons.mozilla.org/en-US/firefox/addon/violentmonkey/)  
+
+2\. Install [9anime Watch-List Custom Filter](https://greasyfork.org/en/scripts/451209-9anime-downloader)\.  
+3\. Optional: It's Recommended to Use AdBlocker Extension. Like [Adblock Plus](https://chrome.google.com/webstore/detail/adblock-plus-free-ad-bloc/cfhdojbkjhnklbpkdaibdccddilifddb) or [AdGuard](https://chrome.google.com/webstore/detail/adguard-adblocker/bgnkhhnnamicmpeenaelnjfhikgbkllg) .  
+
+Done.
+
+&nbsp;
+
+* * *
+
+&nbsp;  
+### How to use:
+
+1\. Go to 9anime.to or any other supported sites. Then access Watch-List by clicking the profile icon on the top-right corner of the screen.  
+
+&nbsp;  
+&nbsp;  
+2\. Wait for the website to finishing loading.
+
+&nbsp;  
+&nbsp;  
+3\. After that you can see the Custom Filter menu.
+
+<img src="https://raw.githubusercontent.com/Fantasy-Boss/9anime-Downloader/main/images/how-to-use-3.png" alt="9anime Downloader" width="70%"> 
+
+&nbsp;  
+&nbsp;  
+4\. Completed (Sub) or Completed (Dub) means that all episodes has been finished airing for those series in Subbed or Dubbed.
+
+<img src="https://raw.githubusercontent.com/Fantasy-Boss/9anime-Downloader/main/images/how-to-use-4.png" alt="9anime Downloader" width="70%">  
+&nbsp;  
+
+5\. Releasing (Sub) or Releasing (Dub) means that this series is still ongoing in Subbed or Dubbed.
+&nbsp;  
+&nbsp;
+
+6\. Reset Filter means resetting  only the Custom Filter.
+
+* * *
+
+### About
+
+This script is dedicated to those anime suckers out there. **Long Live Anime.**  
+
+Any kind of feedback would be helpful. Post any Issues or Suggestions here - [Github](https://github.com/Fantasy-Boss/9anime-Downloader/issues/new).  
+
+If you want to support me then you can **[Buy me a coffee](https://www.buymeacoffee.com/FantasyBoss)**.  
+&nbsp;  
+
+* * *
+
+### Disclaimer
+
+This script or I do not take any legal responsibility.  
+&nbsp;  
+&nbsp;  
+&nbsp;  
+&nbsp;
